f696604061d43166caf1452442ff81f16dd3177c
andrewphan865@gmail.com <andrewphan865@gmail.com>
2024-06-17 21:36:28 +0930

    add RabbitMQ, MassTransit and create some consumers

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -3,6 +3,8 @@ using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Contracts;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,11 +16,14 @@ public class AuctionsController : ControllerBase
 {
     private readonly AuctionDbContext _context;
     private readonly IMapper _mapper;
+    private readonly IPublishEndpoint _publishEndpoint;
 
-    public AuctionsController(AuctionDbContext context, IMapper mapper)
+    public AuctionsController(AuctionDbContext context, IMapper mapper,
+        IPublishEndpoint publishEndpoint)
     {
         _context = context;
         _mapper = mapper;
+        _publishEndpoint = publishEndpoint;
     }
 
     [HttpGet]
@@ -54,13 +59,15 @@ public class AuctionsController : ControllerBase
         auction.Seller = "test";
 
         _context.Auctions.Add(auction);
+        var newAuction = _mapper.Map<AuctionDto>(auction);
+
+        await _publishEndpoint.Publish(_mapper.Map<AuctionCreated>(newAuction));
 
         var result = await _context.SaveChangesAsync() > 0;
-
         if (!result) return BadRequest("Could not save changes to the DB");
 
         return CreatedAtAction(nameof(GetAuctionById),
-            new {auction.Id}, _mapper.Map<AuctionDto>(auction));
+            new {auction.Id}, newAuction);
     }
 
     [HttpPut("{id}")]
@@ -79,6 +86,8 @@ public class AuctionsController : ControllerBase
         auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
         auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
 
+        await _publishEndpoint.Publish(_mapper.Map<AuctionUpdated>(auction));
+
         var result = await _context.SaveChangesAsync() > 0;
 
         if (result) return Ok();
@@ -96,6 +105,8 @@ public class AuctionsController : ControllerBase
         // TODO: check seller == username
 
         _context.Auctions.Remove(auction);
+
+         await _publishEndpoint.Publish<AuctionDeleted>(new { Id = auction.Id.ToString() });
 
         var result = await _context.SaveChangesAsync() > 0;
 
---
+++ src/SearchService/Consumers/AuctionCreatedConsumer.cs
@@ -0,0 +1,27 @@
+using AutoMapper;
+using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+
+namespace SearchService;
+
+public class AuctionCreatedConsumer : IConsumer<AuctionCreated>
+{
+    private readonly IMapper _mapper;
+
+    public AuctionCreatedConsumer(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+
+    public async Task Consume(ConsumeContext<AuctionCreated> context)
+    {
+        Console.WriteLine("--> Consuming auction created: " + context.Message.Id);
+
+        var item = _mapper.Map<Item>(context.Message);
+        // test fail case
+        //if (item.Model == "Foo") throw new ArgumentException("Cannot sell cars with name of Foo");
+
+        await item.SaveAsync();
+    }
+}
